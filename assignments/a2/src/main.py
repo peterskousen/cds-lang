b7a30b0e8d3d89cbf48174a836da224925ae7fd9
peterskousen <phskousen@gmail.com>
2024-03-17 11:47:48 +0100

    updated a2 files

--- assignments/a2/src/main.py
+++ assignments/a2/src/main.py
@@ -9,5 +9,28 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import nn_classifier as nn
 import lrc_classifier as lrc
+import argparse
 
-nn.processor()
+def get_program_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--classification_type','-cl',
+                        required = True,
+                        help = "Type of classifier to execute"
+    )
+    parser.add_argument('--input_sentence','-i',
+                        help = "Input sentence to analyse",
+                        choices = ["nn", "lrc"],
+                        required = True,
+                        type = str
+    )
+    return parser.parse_args()
+
+def main():
+    get_program_args()
+
+if __name__=="__main__":
+    args = get_program_args()
+    if args.classification_type == "nn":
+        nn.eval_sentence(args.input_sentence)
+    elif args.classification_type == "lrc":
+        lrc.eval_sentence(args.input_sentence)
