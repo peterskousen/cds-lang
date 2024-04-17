ae2a14a8530ff245a98882705b198a4bb123c4aa
peterskousen <phskousen@gmail.com>
2024-04-17 13:58:43 +0200

    added a4

--- assignments/a4/setup.sh
+++ assignments/a4/setup.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
-# create virtual env
 python -m venv .venv
 
-#activate env
 source ./.venv/bin/activate
 
-# requirements
 pip install --upgrade pip
 pip install -r requirements.txt
 
